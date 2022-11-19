36332733125d1ebaebb5b5eacd73505350761ea4
Max Morgenbesser <maxwellmorgenbesser@gmail.com>
2022-11-18 22:01:45 -0500

    pracgticed getting location continuously with expo

--- App.js
+++ App.js
@@ -1,10 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Location from 'expo-location';
+import { useState,useEffect } from 'react';
 
 export default function App() {
+const [location,setLocation] = useState(null)
+
+useEffect(() => {
+    (async () => {
+      
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    })();
+  }, [location]);
+
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      {location&& console.log(location)}
       <StatusBar style="auto" />
     </View>
   );
