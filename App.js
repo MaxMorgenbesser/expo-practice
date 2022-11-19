a8c1a5cc908b589cb666083079d8ac6f2307a5da
Max Morgenbesser <maxwellmorgenbesser@gmail.com>
2022-11-18 23:02:52 -0500

    had it update the location every ten seconds

--- App.js
+++ App.js
@@ -1,32 +1,61 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Dimensions,Image } from 'react-native';
 import * as Location from 'expo-location';
-import { useState,useEffect } from 'react';
+import MapView, {Marker} from 'react-native-maps';
+import { useState,useEffect,  } from 'react';
+
 
 export default function App() {
 const [location,setLocation] = useState(null)
+const [userCoordinates,setUserCoordinates] = useState(null)
 
-useEffect(() => {
+// useEffect(() => {
+ const getLocation=()=>{
+
     (async () => {
-      
+      console.log(" --------- /n it has been 10 seconds")
       let { status } = await Location.requestForegroundPermissionsAsync();
       if (status !== 'granted') {
         setErrorMsg('Permission to access location was denied');
         return;
       }
-
       let location = await Location.getCurrentPositionAsync({});
       setLocation(location);
     })();
-  }, [location]);
+  }
+    
+  // }, []);
+  useEffect(()=>{
+    getLocation()
+  },[])
 
+      useEffect(()=>{
+        setUserCoordinates({
+          latitude: location?.coords?.latitude,
+          longitude: location?.coords?.longitude,
+          latitudeDelta: 0.04,
+          longitudeDelta: 0.04,
+        });
+       
+      },[location]) 
 
+      {setTimeout(getLocation, 10000)}
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      {location&& console.log(location)}
-      <StatusBar style="auto" />
-    </View>
+    <>
+    
+{location && userCoordinates &&
+    <View style={mapstyles.container}>
+    <MapView
+      style={mapstyles.map}
+      initialRegion={userCoordinates} //your region data goes here.
+    >
+      {/*Make sure the Marker component is a child of MapView. Otherwise it won't render*/}
+      <Marker coordinate={userCoordinates} ></Marker>
+    </MapView>
+    {location && console.log(location)}
+  </View>
+}
+</>
   );
 }
 
@@ -38,3 +67,16 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+const mapstyles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  map: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+  },
+})
